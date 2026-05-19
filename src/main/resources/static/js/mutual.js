60f80d8145721736623c02ad1aec38422692a2e2
HS <125457468+hyunseo0504@users.noreply.github.com>
2026-05-19 12:48:10 +0900

    chat/create

--- src/main/resources/static/js/mutual.js
+++ src/main/resources/static/js/mutual.js
@@ -18,7 +18,7 @@ function initMutualChatButtons() {
             console.log('상대(userNo):', userNo);
             
             // 2. 주소 조립 및 이동 (변수가 비어있어도 에러 안 나게 처리)
-            let targetUrl = '/chatroom/create';
+            let targetUrl = '/chat/create';
             
             if (userNo && currentUserNo) {
                 targetUrl += '?targetUserNo=' + userNo
